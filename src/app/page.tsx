4baa5fe4064c3414c94e07613f9d1582d5a5d3a1
masud-rana44 <mdmasudrana4488@gmail.com>
2025-01-26 17:12:05 +0600

    Optimized horizontal scroll behavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Page() {
     inventory: [] as Array<RefObject<VariableSizeGrid>>,
   });
 
-  // Optimize scroll handler by using a single function
+  // Optimize scroll handler
   const handleScroll = useCallback(({ scrollLeft }: { scrollLeft: number }) => {
     const { calenderMonths, calenderDates, inventory } = scrollRefs.current;
 
